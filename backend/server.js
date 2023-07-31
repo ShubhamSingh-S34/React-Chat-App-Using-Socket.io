88b0c83086a20ba8d344ee7c1036a037d28c629f
shubham <singhshubham3006.ss@gmail.com>
2023-07-31 18:33:12 +0530

    Initial commit | Read description for more info

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "*",
+    }
+});
+
+
+io.on('connection', (socket) => {
+    console.log(socket);
+    console.log("Socket is active");
+    socket.on("chat", (payload) => {
+        console.log(payload);
+        io.emit("chat", payload);
+    })
+
+})
+
+
+
+// app.listen(5000, () => {
+//     console.log("Listinening to port 5000")
+// })
+server.listen(5000, () => {
+    console.log("Listinening to port 5000")
+})
